c831006b0bc803fd445fce523024743fdca3b4c6
Fahmiiiiiiii <100488113+Fahmiiiiiiii@users.noreply.github.com>
2023-03-30 14:07:18 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const sessionName = "yusril";
-const donet = "https://saweria.co/sansekai";
-const owner = ["6287878817169"]; // Put your number here ex: ["62xxxxxxxxx"]
+const donet = "";
+const owner = ["6285280407526"]; // Put your number here ex: ["62xxxxxxxxx"]
 const {
   default: sansekaiConnect,
   useMultiFileAuthState,
